f17ff3d7d2b801e1490b396912b97607772c4bf2
Dave Irwin <davidjoseph.irwin@gmail.com>
2019-01-16 10:38:46 -0500

    Changing some colors

--- CycleBackground.rb
+++ CycleBackground.rb
@@ -5,10 +5,10 @@ currentWallpaperFile = "#{__dir__}/currentWallpaper.txt"
 assets = "#{__dir__}/assets"
 
 map = {
-  "deadcells_1.png" => ["#111D0D", "#98FFDE", "#2C4866"],
-  "deadcells_2.png" => ["#0B101A", "#98BED0", "#133A4E"],
-  "deadcells_3.png" => ["#0C0F2D", "#E58BA1", "#3D2C50"],
-  "deadcells_4.jpg" => ["#141B2E", "#EAE1D1", "#292147"],
+  "deadcells_1.png" => ["#111D0D", "#C4C0C2", "#2C4866"],
+  "deadcells_2.png" => ["#0B101A", "#BCDAE3", "#133A4E"],
+  "deadcells_3.png" => ["#0C0F2D", "#BB83A0", "#3D2C50"],
+  "deadcells_4.jpg" => ["#141B2E", "#EAE1D1", "#402B70"],
   "deadcells_5.png" => ["#221720", "#B6AB95", "#4C304B"],
   "secret_island.png" => ["#322439","#FBCFAA","#7B658D"],
   "city_bridge_view.jpg" => ["#3B4C56", "#F4F4F6", "#ED9F9D"]
