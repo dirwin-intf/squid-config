8dc2735e409f1faabf0576168fc7b614d152be7f
Dave Irwin <davidjoseph.irwin@gmail.com>
2019-02-06 15:22:47 -0500

    Delegate color replacement to ReplaceColors.rb in Barcycle and i3cycle

---
+++ Barcycle/ReplaceColors.rb
@@ -0,0 +1,27 @@
+require '../PalletteMap.rb'
+
+module ReplacePolybarColors
+  def self.replaceColors newWallpaper
+    colorFilePath = "#{__dir__}/conf/color"
+
+    # grab all the old values so we can invoke sed later and replace them
+    command = "grep -e \"master-background = *\" #{colorFilePath}"
+    oldBackground = `#{command}`.chomp
+
+    command = "grep -e \"master-foreground = *\" #{colorFilePath}"
+    oldForeground = `#{command}`.chomp
+
+    command = "grep -e \"master-alternate = *\" #{colorFilePath}"
+    oldAlternate = `#{command}`.chomp
+
+    # build up our replacement sed expressions
+    newBackground = "master-background = " + PalletteMap::palletteMap[newWallpaper][0]
+    newForeground = "master-foreground = " + PalletteMap::palletteMap[newWallpaper][1]
+    newAlternate = "master-alternate = " + PalletteMap::palletteMap[newWallpaper][2]
+
+    command = "sed -i -e \"s/#{oldBackground}/#{newBackground}/g\" "\
+      + "-e \"s/#{oldForeground}/#{newForeground}/g\" "\
+      + "-e \"s/#{oldAlternate}/#{newAlternate}/g\" #{colorFilePath}"
+    `#{command}`
+  end
+end
---
+++ i3cycle/ReplaceColors.rb
@@ -0,0 +1,46 @@
+require '../PalletteMap.rb'
+
+module ReplaceI3Colors
+  def self.replaceColors newWallpaper
+    colorFilePath = "#{__dir__}/conf/color"
+
+    # grab the old values for
+    # client.focused, client.focused_inactive, and client.unfocused
+    command = "grep -e \"client.focused #*\" #{colorFilePath}"
+    oldFocused = `#{command}`.chomp
+    oldFocusedArr = oldFocused.split(' ');
+
+    command = "grep -e \"client.focused_inactive #*\" #{colorFilePath}"
+    oldInactiveFocused = `#{command}`.chomp
+    oldInactiveFocusedArr = oldInactiveFocused.split(' ');
+
+    command = "grep -e \"client.unfocused #*\" #{colorFilePath}"
+    oldUnfocused = `#{command}`.chomp
+    oldUnfocusedArr = oldUnfocused.split(' ');
+
+    masterBackground = PalletteMap::palletteMap[newWallpaper][0]
+    masterForeground = PalletteMap::palletteMap[newWallpaper][1]
+    masterAlternate = PalletteMap::palletteMap[newWallpaper][2]
+
+    # replace the background and text, of focused and focused_inactive
+    oldFocusedArr[2] = masterAlternate
+    oldFocusedArr[3] = masterForeground
+
+    # replace the background and text of focused_inactive
+    oldInactiveFocusedArr[2] = masterAlternate
+    oldInactiveFocusedArr[3] = masterForeground
+
+    # replace the background of unfocused
+    oldUnfocusedArr[2] = masterBackground
+
+    # create the new values to be used in sed
+    newFocused = oldUnfocusedArr.join(' ')
+    newInactiveFocused = oldInactiveFocusedArr.join(' ')
+    newUnfocused = oldUnfocusedArr.join(' ')
+
+    command = "sed -i -e \"s/#{oldFocused}/#{newFocused}/g\" "\
+      + "-e \"s/#{oldInactiveFocused}/#{newInactiveFocused}/g\" "\
+      + "-e \"s/#{oldUnfocused}/#{newUnfocused}/g\" #{colorFilePath}"
+    `#{command}`
+  end
+end
