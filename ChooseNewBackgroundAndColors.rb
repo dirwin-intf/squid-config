f4e19a78317805d3e55d01bd4b914673c9c9178d
Dave Irwin <davidjoseph.irwin@gmail.com>
2019-02-06 15:22:47 -0500

    Delegate color replacement to ReplaceColors.rb in Barcycle and i3cycle

--- ChooseNewBackgroundAndColors.rb
+++ ChooseNewBackgroundAndColors.rb
@@ -1,24 +1,12 @@
+require './PalletteMap.rb'
+require './Barcycle/ReplaceColors.rb'
+require './i3cycle/ReplaceColors.rb'
+
 # set constants
-polybarConfig = "~/.config/polybar/config"
 currentWallpaperFile = "#{__dir__}/currentWallpaper.txt"
 assets = "#{__dir__}/assets"
 
-# key value pairs in the form of { filename, [backgroundColor, fontColor, highlightColor] }
-# this is used when invoking sed later
-palletteMap = {
-}
-
-# grab all the old values so we can invoke sed later and replace them
-command = "grep -e \"master-background = *\" #{polybarConfig}"
-oldBackground = `#{command}`.chomp
-
-command = "grep -e \"master-foreground = *\" #{polybarConfig}"
-oldForeground = `#{command}`.chomp
-
-command = "grep -e \"master-alternate = *\" #{polybarConfig}"
-oldAlternate = `#{command}`.chomp
-
-if ARGV[0] != nil and palletteMap[ARGV[0]] != nil
+if ARGV[0] != nil and PalletteMap::palletteMap[ARGV[0]] != nil
   newWallpaper = ARGV[0]
 else
   # grab the current wallpaper so we can make sure the new one that is
@@ -34,18 +22,11 @@ else
   end
 end
 
+ReplacePolybarColors.replaceColors(newWallpaper)
+ReplaceI3Colors.replaceColors(newWallpaper)
+
 # write the current wallpaper to file for posterity
 command = "echo \"#{newWallpaper}\" > #{currentWallpaperFile}"
-`#{command}`
-
-# build up our replacement sed expressions
-newBackground = "master-background = " + palletteMap[newWallpaper][0]
-newForeground = "master-foreground = " + palletteMap[newWallpaper][1]
-newAlternate = "master-alternate = " + palletteMap[newWallpaper][2]
-
-command = "sed -i -e \"s/#{oldBackground}/#{newBackground}/g\" "\
-               + "-e \"s/#{oldForeground}/#{newForeground}/g\" "\
-               + "-e \"s/#{oldAlternate}/#{newAlternate}/g\" #{polybarConfig}"
 `#{command}`
 
 # echo the path to the new wallpaper so that nitrogen can select it
