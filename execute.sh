65fd85534151edf3d7e6a72fc3d344e5f81e2f0a
Dave Irwin <davidjoseph.irwin@gmail.com>
2019-02-06 13:45:33 -0500

    Delegate config replacement to concatenate.sh

--- execute.sh
+++ execute.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 dir=`dirname "$0"`
 
+# if there is no current wallpaper just create a place holder file so ChooseNewBackgroundAndColors doesn't blow up
 [ ! -f $dir/currentWallpaper.txt ] && touch $dir/currentWallpaper.txt && echo "placeholder" > $dir/currentWallpaper.txt
 
 newbg=`ruby $dir/ChooseNewBackgroundAndColors.rb $1`
 
 nitrogen $newbg --set-zoom-fill
-pkill polybar &> /dev/null
-polybar bottom &> /dev/null &
-polybar top &> /dev/null &
+bash $dir/Barcycle/concatenate.sh
+bash $dir/i3cycle/concatenate.sh
