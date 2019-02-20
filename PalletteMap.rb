fa5293b61a8185cd4875cf18d3512056d3131720
Dave Irwin <davidjoseph.irwin@gmail.com>
2019-02-20 09:34:38 -0500

    Adjust color mappings

--- PalletteMap.rb
+++ PalletteMap.rb
@@ -11,12 +11,12 @@ module PalletteMap
       "city_bridge_view.jpg" =>   ["#3B4C56", "#F4F4F6", "#ED9F9D"],
       "autumn_scene.png" =>       ["#292F4E", "#C0B58C", "#4B5669"],
       "blue_view.png" =>          ["#102438", "#EAFDF7", "#226375"],
-      "hyperlight_drifter.png" => ["#2A1F31", "#BBC7C9", "#5F2B53"],
+      "hyperlight_drifter.png" => ["#2A1F31", "#BBC7C9", "#254B6E"],
       "snowy_forest.png" =>       ["#13102F", "#9DBCDD", "#2D4270"],
       "great_wave.png" =>         ["#070B1A", "#D7C9AA", "#535D5C"],
       "fighting_sleep.png" =>     ["#271F37", "#EAE7EB", "#8C4E6C"],
       "dock.jpg" =>               ["#343A53", "#F0E9D7", "#2D989C"],
-      "overgrown_arena.png" =>     ["#22262E", "#C5B585", "#5D7333"],
+      "overgrown_arena.png" =>    ["#22262E", "#C5B585", "#5D7333"],
       "dragons_keep.png" =>       ["#291508", "#FFF396", "#963A09"],
       "farm.png" =>               ["#401212", "#FDEE91", "#6D4A0D"]
     }
